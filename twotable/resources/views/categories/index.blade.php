386eee22f622abd85431497fbc979f5b6a89337a
LucaIlie <ilie.luca@elev.cihcahul.md>
2021-02-25 12:45:14 +0200

    Setare Rute

--- twotable/resources/views/categories/index.blade.php
+++ twotable/resources/views/categories/index.blade.php
@@ -8,7 +8,17 @@
             <div class="col-md-6 m-auto">
                 <div class="form-group">
                     <label>Nume:</label>
+                    <input type="text" name="nume" class="form-control">
                 </div>
+            </div>
+            <div class="col-md-6 m-auto">
+                <div class="form-group">
+                    <label>Sector:</label>
+                    <input type="text" name="sector" class="form-control">
+                </div>
+            </div>
+            <div class="form-group">
+                <input type="submit" value="Save" class="form-control">
             </div>
         </div>
     </form>
--- twotable/routes/web.php
+++ twotable/routes/web.php
@@ -2,5 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
+
 
 Route::get('/',[PageController::class,'index'])->name('index');
+Route::resource('category',CategoryController::class);
+Route::resource('product',ProductController::class);
